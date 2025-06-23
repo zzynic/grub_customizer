91581c71b25888ffd55fc68623956cb3ce5aa6d0
zzynic <zzynikk@outlook.com>
2025-06-23 19:27:30 +0300

    Update install_grub_customizer_ubuntu.sh

--- install_grub_customizer_ubuntu.sh
+++ install_grub_customizer_ubuntu.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status
 set -e
 
-echo "Updating package list..."
+echo "Installing dependencies..."
 sudo apt update
+sudo apt install -y cmake build-essential libgtkmm-3.0-dev gettext libssl-dev libarchive-dev git
+
+echo "Cloning grub-customizer source code..."
+git clone https://github.com/danielrichter2007/grub-customizer.git
+cd grub-customizer
+
+echo "Building grub-customizer..."
+cmake .
+make -j$(nproc)
 
 echo "Installing grub-customizer..."
-sudo apt install -y grub-customizer
+sudo make install
 
-echo "Installation complete!"
+echo "Done. You can now run grub-customizer from your menu or by typing 'grub-customizer'."
